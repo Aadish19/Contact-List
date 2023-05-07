6e925c8b1339a95134c714a7f0cab8eda6398a5f
Aadish19 <adish8511@gmail.com>
2023-05-07 11:09:13 +0530

    Changes function to promises according to new version of mongoose v7

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const Contact = require('./models/contacts')
 
 const app = express();
 
-app.set('view engine','ejs');   // ejs  provided to view engine
-app.set('views',path.join(__dirname,'views'))   //in predefined keyword "views" we give our ejs(html) file's folder
+app.set('view engine', 'ejs');   // ejs  provided to view engine
+app.set('views', path.join(__dirname, 'views'))   //in predefined keyword "views" we give our ejs(html) file's folder
 
 app.use(express.urlencoded()); // middleware
 app.use(express.static('assets')); // css and js handler
@@ -23,42 +23,67 @@ app.use(express.static('assets')); // css and js handler
 //     }
 // ]
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
 
-    Contact.find({},function(err,contacts){
-        if(err){
-            console.error(console,"Error in fetching contact details");
-            return;
-        }
+    // Contact.find({},function(err,contacts){
+    //     if(err){
+    //         console.error(console,"Error in fetching contact details");
+    //         return;
+    //     }
 
-        return res.render('home',{
-            title:'Home Page' , 
-            contact_list : contacts
+    //     return res.render('home',{
+    //         title:'Home Page' , 
+    //         contact_list : contacts
+    //     })
+    // })
+
+    Contact.find({})
+        .then(contacts => {
+            return res.render('home', {
+                title: 'Home Page',
+                contact_list: contacts
+            });
         })
-    })
-    
+        .catch(error => {
+            console.error('Error in fetching contact details', error);
+            return;
+        });
+
 })
 
-app.get('/practice',function(req,res){
-    return res.render('practice',{
-        title:'My practice page'
+app.get('/practice', function (req, res) {
+    return res.render('practice', {
+        title: 'My practice page'
     })
 })
 
-app.post('/create-contact',function(req,res){
-    
+app.post('/create-contact', function (req, res) {
+
+    // Contact.create({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // }, function (err, newContact) {
+    //     if (err) {
+    //         console.log("error in creating a contact");
+    //         return;
+    //     }
+
+    //     console.log('*******', newContact);
+    //     return res.redirect('back')
+    // })
+
     Contact.create({
         name: req.body.name,
         phone: req.body.phone
-    },function(err, newContact){
-        if(err){
-            console.log("error in creating a contact");
-            return;
-        }
-
-        console.log('*******',newContact);
-        return res.redirect('back')
     })
+        .then(newContact => {
+            console.log('*******', newContact);
+            return res.redirect('back');
+        })
+        .catch(error => {
+            console.log('error in creating a contact', error);
+            return;
+        });
 
 })
 
@@ -72,26 +97,33 @@ app.post('/create-contact',function(req,res){
 // })
 
 //query params
-app.get('/delete-contact/',function(req,res){
+app.get('/delete-contact/', function (req, res) {
     //get the id from query in the ul
     let id = req.query.id;
 
     //find the contact in the database using id and delete
-    Contact.findByIdAndDelete(id,function(err){
-        if(err){
-            console.log("Error in deleting an object from database");
+    // Contact.findByIdAndDelete(id, function (err) {
+    //     if (err) {
+    //         console.log("Error in deleting an object from database");
+    //         return;
+    //     }
+
+    //     return res.redirect('back')
+    // })
+
+    Contact.findByIdAndDelete(id)
+        .then(() => {
+            return res.redirect('back');
+        })
+        .catch(error => {
+            console.log('Error in deleting an object from database', error);
             return;
-        }
-
-        return res.redirect('back')
-    })
-
-
+        });
 })
 
-app.listen(port,function(err){
-    if(err){
-        console.log("Error Occured",err);
+app.listen(port, function (err) {
+    if (err) {
+        console.log("Error Occured", err);
         return;
     }
 
